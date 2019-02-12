5113f8bb2ff609ae3c67f1e16a52850fff43d65d
S M Y <smy.altamash@gmail.com>
2019-02-12 17:46:41 +0530

    Issue #000 category: categories creation

---
+++ masterCategories.sh
@@ -0,0 +1,122 @@
+curl -X POST \
+  https://sunbirditer6.tk/action/framework/v3/category/master/create \
+  -H 'Authorization: Bearer API_KEY' \
+  -H 'Content-Type: application/json' \
+  -H 'X-Channel-Id: 0126971656346091520' \
+  -H 'cache-control: no-cache' \
+  -H 'user-id: 1a703df9-d07d-4609-a93c-6be262a8c754' \
+  -d '{
+   "request": {
+      "category":{
+          "name":"Board",
+          "description":"board",
+          "code":"board"
+      }
+    }
+}'
+
+curl -X POST \
+  https://sunbirditer6.tk/action/framework/v3/category/master/create \
+  -H 'Authorization: Bearer API_KEY' \
+  -H 'Content-Type: application/json' \
+  -H 'X-Channel-Id: 0126971656346091520' \
+  -H 'cache-control: no-cache' \
+  -H 'user-id: 1a703df9-d07d-4609-a93c-6be262a8c754' \
+  -d '{
+   "request": {
+      "category":{
+          "name":"Level",
+          "description":"gradeLevel",
+          "code":"gradeLevel"
+      }
+    }
+}'
+
+
+curl -X POST \
+  https://sunbirditer6.tk/action/framework/v3/category/master/create \
+  -H 'Authorization: Bearer API_KEY' \
+  -H 'Content-Type: application/json' \
+  -H 'X-Channel-Id: 0126971656346091520' \
+  -H 'cache-control: no-cache' \
+  -H 'user-id: 1a703df9-d07d-4609-a93c-6be262a8c754' \
+  -d '{
+   "request": {
+      "category":{
+          "name":"Subject",
+          "description":"subject",
+          "code":"subject"
+      }
+    }
+}'
+
+curl -X POST \
+  https://sunbirditer6.tk/action/framework/v3/category/master/create \
+  -H 'Authorization: Bearer API_KEY' \
+  -H 'Content-Type: application/json' \
+  -H 'X-Channel-Id: 0126971656346091520' \
+  -H 'cache-control: no-cache' \
+  -H 'user-id: 1a703df9-d07d-4609-a93c-6be262a8c754' \
+  -d '{
+   "request": {
+      "category":{
+          "name":"Medium",
+          "description":"medium",
+          "code":"medium"
+      }
+    }
+}'
+
+
+curl -X POST \
+  https://sunbirditer6.tk/action/framework/v3/category/master/create \
+  -H 'Authorization: Bearer API_KEY' \
+  -H 'Content-Type: application/json' \
+  -H 'X-Channel-Id: 0126971656346091520' \
+  -H 'cache-control: no-cache' \
+  -H 'user-id: 1a703df9-d07d-4609-a93c-6be262a8c754' \
+  -d '{
+   "request": {
+      "category":{
+          "name":"Topic",
+          "description":"topic",
+          "code":"topic"
+      }
+    }
+}'
+
+
+curl -X POST \
+  https://sunbirditer6.tk/action/framework/v3/category/master/create \
+  -H 'Authorization: Bearer API_KEY' \
+  -H 'Content-Type: application/json' \
+  -H 'X-Channel-Id: 0126971656346091520' \
+  -H 'cache-control: no-cache' \
+  -H 'user-id: 1a703df9-d07d-4609-a93c-6be262a8c754' \
+  -d '{
+   "request": {
+      "category":{
+          "name":"Subtopic",
+          "description":"subtopic",
+          "code":"subtopic"
+      }
+    }
+}'
+
+curl -X POST \
+  https://sunbirditer6.tk/action/framework/v3/category/master/create \
+  -H 'Authorization: Bearer API_KEY' \
+  -H 'Content-Type: application/json' \
+  -H 'X-Channel-Id: 0126971656346091520' \
+  -H 'cache-control: no-cache' \
+  -H 'user-id: 1a703df9-d07d-4609-a93c-6be262a8c754' \
+  -d '{
+   "request": {
+      "category":{
+          "name":"Purpose",
+          "description":"purpose",
+          "code":"purpose"
+      }
+    }
+}'
+
